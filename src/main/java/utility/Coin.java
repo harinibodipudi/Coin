bfccd31db363fd38bb8ee965f2389701b71d9a59
harinimanu <harinimanu123@gmail.com>
2021-04-05 20:23:42 +0530

    method to check equality of heads and tails probabilityof a coin

--- src/main/java/utility/Coin.java
+++ src/main/java/utility/Coin.java
@@ -1,5 +1,9 @@
 package utility;
 
 public class Coin {
-
+    double probabilityOfHead = 0.5;
+    double probabilityOfTails = 0.5;
+     public boolean toCheckHeadProbabilityIsEqualToTailProbability(){
+         return probabilityOfHead==probabilityOfTails;
+     }
 }
--- src/test/java/utility/TestCoin.java
+++ src/test/java/utility/TestCoin.java
@@ -1,5 +1,14 @@
 package utility;
 
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.Assertions;
 public class TestCoin {
     Coin coin = new Coin();
+    @Test
+    public void testProbabilityOfHeadIsEqualToThatOfTailsWhenACoinIsFlipped(){
+        boolean actualValue=coin.toCheckHeadProbabilityIsEqualToTailProbability();
+        boolean expectedValue = true;
+
+
+    }
 }
