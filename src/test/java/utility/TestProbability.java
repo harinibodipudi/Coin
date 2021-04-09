7f58252bd2e7f1fdf844556e4d2b6c13638b2849
harinimanu <harinimanu123@gmail.com>
2021-04-09 13:35:41 +0530

    renamed to probability from coinprobability

---
+++ src/test/java/utility/TestProbability.java
@@ -0,0 +1,54 @@
+package utility;
+
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.Assertions;
+
+import static utility.Probability.add;
+
+public class TestProbability {
+
+    @Test
+    public void testProbabilityOfHeadIsEqualToThatOfTailsWhenACoinIsFlipped(){
+        double probability_Head = 0.5;
+        double probability_Tail = 0.5;
+        Probability probability = new Probability(probability_Head,probability_Tail);
+
+        Assertions.assertTrue(probability.equals(probability));
+    }
+    @Test
+    public void testProbabilityForEventToOccurTogetherForHeadAndHeadProbability(){
+         Probability HeadsForFirstCoinToss = new Probability(0.5, 0.5);
+         Probability HeadsForSecondCoinToss = new Probability(0.5,0.5);
+
+         double actualValue = add(HeadsForFirstCoinToss.probabilityOfHead, HeadsForSecondCoinToss.probabilityOfHead);
+
+         Assertions.assertEquals(0.25, actualValue);
+    }
+    @Test
+    public void testProbabilityForEventToOccurTogetherForHeadAndTailProbability(){
+        Probability HeadForFirstCoinToss = new Probability(0.5, 0.5);
+        Probability TailsForSecondCoinToss = new Probability(0.5,0.5);
+
+        double actualValue = add(HeadForFirstCoinToss.probabilityOfHead, TailsForSecondCoinToss.probabilityOfTails);
+
+        Assertions.assertEquals(0.25, actualValue);
+    }
+    @Test
+    public void testProbabilityForEventToOccurTogetherForTailAndHeadProbability(){
+        Probability TailsForFirstCoinToss = new Probability(0.5, 0.5);
+        Probability HeadsForFirstCoinToss = new Probability(0.5,0.5);
+
+        double actualValue = add(TailsForFirstCoinToss.probabilityOfTails, HeadsForFirstCoinToss.probabilityOfHead);
+
+        Assertions.assertEquals(0.25, actualValue);
+    }
+    @Test
+    public void testProbabilityForEventToOccurTogetherForTailAndTailProbability(){
+        Probability TailsForFirstCoinToss = new Probability(0.5, 0.5);
+        Probability TailsForSecondCoinToss = new Probability(0.5,0.5);
+
+        double actualValue = add(TailsForFirstCoinToss.probabilityOfTails, TailsForSecondCoinToss.probabilityOfTails);
+
+        Assertions.assertEquals(0.25, actualValue);
+    }
+}
